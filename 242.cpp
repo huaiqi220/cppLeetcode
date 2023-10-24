fc39acc38edd372500249d6af533276f781bd312
zhuzi <zhuziyang123456@gmail.com>
2023-10-24 22:05:29 +0800

    add 242 and not finished

---
+++ 242.cpp
@@ -0,0 +1,29 @@
+
+#include "iostream"
+#include "vector"
+#include "string"
+#include "map"
+#include "unordered_map"
+
+
+
+//
+//
+//class Solution {
+//public:
+//    bool isAnagram(std::string s, std::string t) {
+//        std::unordered_map<char,int> umap;
+//
+//        for (int i = 0; i < s.length(); i++)
+//        {
+//            char c = s[i];
+//            if (umap.find(c) == true)
+//        }
+//
+//
+//
+//
+//    }
+//};
+
+//从没用过哈希表，这题明天白天用多种解法做一下
