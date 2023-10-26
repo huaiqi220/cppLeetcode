06afe1629371a4de777377c0deeb493d145d8a4a
zhuzi <zhuziyang123456@gmail.com>
2023-10-26 15:59:37 +0800

    练习了快速排序，同时把一些代码中的主函数注释掉

--- 2698.cpp
+++ 2698.cpp
@@ -4,46 +4,46 @@
 #include "string"
 
 
-class Solution {
-public:
-    int punishmentNumber(int n) {
-
-        int punished = 0;
-
-        for (int i = 1; i < n; i++)
-        {
-            int count = pow(i, 2);
-            std::cout << count << std::endl;
-            std::string counts = std::to_string(count);
-            
-            int accu = 0;
-            if (int res = isValid(i,counts))
-            {
-                
-                punished += count;
-            }
-           
-        }
-
-        return punished;
-
-    }
-
-
-    int isValid(int i, std::string std)
-    {
-
-    }
-};
-
-
-int main()
-{
-    Solution solution;
-    solution.punishmentNumber(10);
-
-}
-
+//class Solution {
+//public:
+//    int punishmentNumber(int n) {
+//
+//        int punished = 0;
+//
+//        for (int i = 1; i < n; i++)
+//        {
+//            int count = pow(i, 2);
+//            std::cout << count << std::endl;
+//            std::string counts = std::to_string(count);
+//            
+//            int accu = 0;
+//            if (int res = isValid(i,counts))
+//            {
+//                
+//                punished += count;
+//            }
+//           
+//        }
+//
+//        return punished;
+//
+//    }
+//
+//
+//    int isValid(int i, std::string std)
+//    {
+//
+//    }
+//};
+//
+//
+//int main()
+//{
+//    Solution solution;
+//    solution.punishmentNumber(10);
+//
+//}
+//
 
 
 //针对字符串分割搜索部分必须使用回溯法，后面再做
---
+++ psort_148.cpp
@@ -0,0 +1,87 @@
+#include "vector"
+#include "iostream"
+#include "algorithm"
+#include "string"
+
+
+
+class Solution {
+
+public:
+
+	void QuickSort(std::vector<int>& list, int start, int end)
+	{
+		if (start >= end)
+		{
+			// 到达边界条件，直接输出结果
+			return;
+		}
+		//以第一个元素作为标杆
+		int mark = list[start];
+		int left = start;
+		int right = end - 1;
+
+		while (left <= right)
+		{
+			while (list[left] < mark) {
+				left++;
+			}
+			while (list[right] > mark) {
+				right--;
+			}
+			if (left <= right) {
+				std::swap(list[left], list[right]);
+				left++;
+				right--;
+			}
+		}
+
+		QuickSort(list, start, right + 1);
+		QuickSort(list, left, end);
+
+
+	}
+
+
+
+
+
+
+
+};
+
+
+
+int main() {
+
+	std::vector<int> list;
+	list.push_back(8);
+	list.push_back(9);
+	list.push_back(5);
+	list.push_back(1);
+	list.push_back(3);
+	list.push_back(9);
+	list.push_back(9);
+	list.push_back(2);
+	list.push_back(9);
+	list.push_back(6);
+	list.push_back(7);
+	list.push_back(7);
+	list.push_back(4);
+	list.push_back(11);
+
+
+	Solution solution;
+	solution.QuickSort(list, 0, list.size());
+
+	for (int item : list)
+	{
+		std::cout << item << std::endl;
+	}
+
+
+
+
+
+
+}
