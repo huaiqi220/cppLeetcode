d2318052f8a54cb20b2c23e01a736f89ad110158
zhuzi <zhuziyang123456@gmail.com>
2023-10-30 18:48:41 +0800

    347这个题目舍近求远，用了一些复杂方法，但也学到了新东西。 学到了结构体的定义，结构体的运算符重载，vector<struct>的自定义sort 可以多复习这个。

---
+++ 347.cpp
@@ -0,0 +1,50 @@
+
+#include "vector"
+#include "map"
+#include "algorithm"
+
+
+class Solution {
+
+    struct aotm
+    {
+        int number;
+        int count;
+        bool operator<(const aotm& other) const {
+            return count > other.count;  // 以value为例进行排序
+        }
+    };
+
+public:
+    std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+        std::map<int, int> countMap;
+
+
+        for (auto item : nums) countMap[item]++;
+        std::vector<aotm> countAotm;
+        for (auto item : countMap)
+        {
+            aotm a;
+            a.number = item.first;
+            a.count = item.second;
+            countAotm.push_back(a);
+        }
+        std::sort(countAotm.begin(),countAotm.end());
+        return getRes(countAotm, k);
+
+    }
+
+    std::vector<int> getRes(std::vector<aotm> countAotm, int k)
+    {
+        std::vector<int> result;
+        for (int i = 0; i < k; i++)
+        {
+            result.push_back(countAotm[i].number);
+        }
+        return result;
+
+    }
+    
+
+
+};
