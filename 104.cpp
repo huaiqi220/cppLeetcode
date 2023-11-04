10a35e7be21c41f5db5836b34dbfcea25be11420
zhuzi <zhuziyang123456@gmail.com>
2023-11-04 14:49:10 +0800

    add 104，非常简单，但不一定是最简单方法

---
+++ 104.cpp
@@ -0,0 +1,51 @@
+
+
+
+
+#include "vector"
+#include "unordered_map"
+
+
+
+
+class Solution {
+
+    struct TreeNode {
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    };
+
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        std::unordered_map<TreeNode*, int> myMap;
+        backTracking(root, 0, myMap);
+        int maxLevel = 0;
+        for (auto item : myMap)
+        {
+            int level = item.second;
+            if (level > maxLevel) maxLevel = level;
+        }
+        return maxLevel + 1;
+
+    }
+
+
+    void backTracking(TreeNode* root, int level, std::unordered_map<TreeNode*, int>& map)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        backTracking(root->left, level + 1, map);
+        map[root] = level;
+        backTracking(root->right, level + 1, map);
+    }
+};
