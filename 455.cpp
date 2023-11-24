cb7801f23c06c4de90fa6734f4d95f9aecf0fe82
zhuzi <zhuziyang123456@gmail.com>
2023-11-24 20:07:36 +0800

    贪心第一题，455分饼干，虽然做出来了，但还是不太理解为什么必须从小开始分配，从大不行吗

---
+++ 455.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include "vector"
+#include <algorithm>
+#include <iostream>
+
+
+class Solution {
+public:
+    int findContentChildren(std::vector<int>& g, std::vector<int>& s) {
+        int num(0);
+        std::sort(g.begin(), g.end(),std::greater<>());
+        std::sort(s.begin(), s.end(), std::greater<>());
+        
+        for (auto item : g) {
+            std::vector<int>::iterator be = s.begin();
+            std::vector<int>::iterator en = s.end();
+            
+            while (be != en) {
+                if (*be >= item) {
+                    num++;
+                    s.erase(be);
+                    break;
+
+                }
+                else
+                {
+                    be++;
+                }
+            }
+            // 想减小计算量，结果弄巧成拙
+            // return num;
+
+        }
+        return num;
+        
+
+    }
+};
+
+//贪心第一题，455分饼干，虽然做出来了，但还是不太理解为什么必须从小开始分配，从大不行吗
