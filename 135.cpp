4c619dc709a9778daee8e3c490680a31059e7f0b
zhuzi <zhuziyang123456@gmail.com>
2023-11-27 14:44:55 +0800

    135 左往右，右往左两次扫描，想不出来

--- 135.cpp
+++ 135.cpp
@@ -8,6 +8,29 @@
 
 class Solution {
 public:
+
+    int candy(std::vector<int>& ratings) {
+        std::vector<int> candy(ratings.size(), 1);
+        for (int i = 1; i < ratings.size(); i++)
+        {
+            if (ratings[i] > ratings[i - 1])candy[i] = candy[i - 1] + 1;
+        }
+
+        for (int j = ratings.size() - 1; j >= 0; j--)
+        {
+            if (ratings[j - 1] > ratings[j])candy[j - 1] = std::max(candy[j - 1], candy[j] + 1);
+        }
+
+        int count(0);
+        for (int i = 0; i < ratings.size(); i++)
+        {
+            count += candy[i];
+        }
+        return count;
+
+    }
+    // 两次优化的巧妙方法。
+
     //int candy(std::vector<int>& ratings) {
     //    // one child one  candy
     //    if (ratings.size() == 1) return 1;
