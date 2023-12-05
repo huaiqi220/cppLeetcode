e187a9908e7e019e0768792469586cef67cb4291
zhuzi <zhuziyang123456@gmail.com>
2023-12-05 16:51:17 +0800

    add 110

---
+++ 110.cpp
@@ -0,0 +1,40 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <cmath>
+#include <algorithm>
+
+class Solution {
+    struct TreeNode {
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode() :val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int i):val(i), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    };
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) return true;
+        else return (isBalanced(root->left) and isBalanced(root->right) and std::abs(getDepth(root->left) - getDepth(root->right)) <= 1);
+    }
+
+
+    int getDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (root) {
+            return std::max(getDepth(root->left), getDepth(root->right)) + 1;
+        }
+    }
+};
