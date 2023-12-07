e70996f3b293eaa37b64cc992673348d18b8ad67
zhuzi <zhuziyang123456@gmail.com>
2023-12-07 20:52:37 +0800

    贪心股票

--- 121.cpp
+++ 121.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 class Solution {
 public:
-    int maxProfit(std::vector<int>& prices) {
+  /*  int maxProfit(std::vector<int>& prices) {
 
         if (prices.size() <= 1) return 0;
         std::vector<int> multi;
@@ -26,7 +26,23 @@ public:
 
         return *std::max_element(dp.begin(), dp.end());
 
+    }*/
+
+    int maxProfit(std::vector<int>& prices) {
+        int curMin = prices[0];
+        int curProfile = 0;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            int today = prices[i];
+            curMin = std::min(curMin, today);
+            int todayProfile = today - curMin;
+            curProfile = std::max(curProfile, todayProfile);
+        }
+        return curProfile;
+
     }
+    //记录在当前之前的最小价格，然后用当天价格减去最小价格，不断往后遍历
+    // 巧妙地蕴含了时间先后，即只能用后高价减去前低价，而不可能相反
 };
 
 // 丑陋的解法，显然跟贪心没有任何关系，反而用的是DP的方法，
--- 53.cpp
+++ 53.cpp
@@ -9,6 +9,14 @@
 #include <algorithm>
 
 class Solution {
+
+    struct Status {
+        int leftSumMax;
+        int rightSumMax;
+        int Max;
+        int mSum;
+    };
+
 public:
     //int maxSubArray(std::vector<int>& nums) {
     //    std::vector<int> dp(nums.size(), 0);
@@ -22,8 +30,18 @@ public:
     //    return *std::max_element(dp.begin(), dp.end());
 
     //}
+    int maxSubArray(std::vector<int>& nums) {
+
+    }
+
+
+
+
 
 
 
 
 };
+//
+//为了保证计算子问题能够按照顺序、不重复地进行，动态规划要求已经求解的子问题不受后续阶段的影响。这个条件也被叫做「无后效性」。换言之，动态规划对状态空间的遍历构成一张有向无环图，遍历就是该有向无环图的一个拓扑序。有向无环图中的节点对应问题中的「状态」，图中的边则对应状态之间的「转移」，转移的选取就是动态规划中的「决策」。
+
