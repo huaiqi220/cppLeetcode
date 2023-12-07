7ffdd4f4793a8f1e3cdbe42a434120f62c71f142
zhuzi <zhuziyang123456@gmail.com>
2023-12-07 20:02:10 +0800

    add 买股票第一题，动态规划，应该贪心思想

--- 121.cpp
+++ 121.cpp
@@ -1,10 +1,33 @@
 
 
-#include "vector"
-
+#include <vector>
 class Solution {
 public:
     int maxProfit(std::vector<int>& prices) {
 
+        if (prices.size() <= 1) return 0;
+        std::vector<int> multi;
+        for (int i = 0; i <= prices.size() - 2; i++)
+        {
+            multi.push_back(prices[i + 1] - prices[i]);
+        }
+
+        return maxSubArray(multi) >= 0 ? maxSubArray(multi) : 0;
+
+    }
+
+    int maxSubArray(std::vector<int>& nums) {
+        std::vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
+        }
+
+        return *std::max_element(dp.begin(), dp.end());
+
     }
 };
+
+// 丑陋的解法，显然跟贪心没有任何关系，反而用的是DP的方法，
+// 如何使用贪心求解，需要继续学习
---
+++ 53.cpp
@@ -0,0 +1,29 @@
+
+
+
+
+// 股票问题给我做差转换成了最大子数组和问题，结果发现最大子数组和问题不会求
+// 做一下子
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    //int maxSubArray(std::vector<int>& nums) {
+    //    std::vector<int> dp(nums.size(), 0);
+    //    dp[0] = nums[0];
+    //    for (int i = 1; i < nums.size(); i++)
+    //    {
+    //        dp[i] = std::max(dp[i - 1] + nums[i], nums[i]);
+    //    }
+
+    //    // 迭代器，*迭代器就可以获得值，迭代器其实就是指向元素的指针
+    //    return *std::max_element(dp.begin(), dp.end());
+
+    //}
+
+
+
+
+};
