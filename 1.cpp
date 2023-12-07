b091e736c7d5376fb0d2a659a098a2a32926a461
zhuzi <zhuziyang123456@gmail.com>
2023-12-07 16:47:58 +0800

    add 1

---
+++ 1.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+class Solution {
+public:
+    //std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    //    std::unordered_map<int, int> map;
+    //    for (int i = 0; i < nums.size(); i++) {
+    //        map[nums[i]] = i;
+    //    }
+
+
+    //    std::sort(nums.begin(), nums.end());
+
+    //    int i = 0;
+    //    int j = nums.size() - 1;
+    //    std::vector<int> res;
+    //    while (i <= j )
+    //    {
+    //        if (nums[i] + nums[j] > target) { j--; continue; }
+    //        if (nums[i] + nums[j] < target) { i++; continue; }
+    //        if (nums[i] + nums[j] == target) {
+    //            res.push_back(map[i]);
+    //            res.push_back(map[j]);
+    //            return res;
+    //        }
+    //    }
+    //    return res;
+
+    //}
+    // ´íÎó·½·¨
+};
