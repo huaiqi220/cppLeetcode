a747ef8076b6f59f1783260f786f791412a39ac2
zhuzi <zhuziyang123456@gmail.com>
2023-12-08 13:36:12 +0800

    add 45

---
+++ 45.cpp
@@ -0,0 +1,47 @@
+
+#include <vector>
+
+
+class Solution {
+public:
+    //int jump(std::vector<int>& nums) {
+    //    std::vector<int> dp(nums.size(), 0);
+    //    for (int i = 0; i <= nums[0]; i++)
+    //    {
+    //        dp[i] = 1;
+    //    }
+    //    if (nums.size() < 1 or nums[0] == 0) {
+    //        return 0;
+    //    }
+    //    if (nums.size() == 1 and nums[0] != 0) {
+    //        return 1;
+    //    }
+    //    int i = 0;
+    //    int j = nums[0];
+    //    while (true) {
+    //        int curMax = 0;
+    //        for (int i = 0; i <= j; i++)
+    //        {
+    //            curMax = i + nums[i] > curMax ? i + nums[i] : curMax;
+    //        }
+
+    //        curMax = curMax > nums.size() - 1 ? nums.size() - 1 : curMax;
+    //        for (int i = j + 1; i <= curMax; i++) {
+    //            dp[i] = dp[j] + 1;
+    //        }
+    //        i = j + 1;
+    //        j = curMax;
+
+    //        if (j == nums.size() - 1) {
+    //            break;
+    //        }
+
+    //    }
+    //    return dp[nums.size() - 1];
+
+
+
+    //}
+
+    // 实现复杂了，扣索引细节浪费时间，不调了，看标准题解
+};
