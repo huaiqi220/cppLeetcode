b3e76d3f09fbd4ca7cd73474fd5bd97d5736ce2e
zhuzi <zhuziyang123456@gmail.com>
2023-12-09 12:59:26 +0800

    提交答案

--- 98.cpp
+++ 98.cpp
@@ -13,37 +13,23 @@ class Solution {
 
 public:
     //bool isValidBST(TreeNode* root) {
+    //    if (root == nullptr)return true;
+
+    //}
+    //bool isValidBST(TreeNode* root ,int maxVal,int minVal,int kind) {
+    //    // maxVal 控制小于上级根结点， minVal控制大于本级根结点,1左子树，2右子树
+    //    if (root == nullptr)return true;
+    //    if(kind == 1)if (root->val < maxVal or root->val > minVal) return false;
+    //    if(kind == 2)if (root->val > maxVal or root->val < minVal) return false;
+    //    
+    //    if(kind == 1){
+    //        if (isValidBST(root->right, maxVal, 2e31 - 1,2) and isValidBST(root->left, maxVal, -2e31,1))
+    //        {
+    //            return true;
+    //        }
+    //    }
+    //    return false;
 
     //}
 
-
-    //bool backTracking(TreeNode* root,int value,char f)
-    //{
-    //    if (root == nullptr)
-    //    {
-    //        return true;
-    //    }
-
-    //    if (f == 'l')
-    //    {
-    //        
-    //    }
-    //    if (f == 'r')
-    //    {
-
-    //    }
-
-
-    //    int val = root->val;
-    //    if (backTracking(root->left, val, 'l') == true and backTracking(root->right, val, 'r') == true)
-    //    {
-    //        return true;
-    //    }
-    //    else
-    //    {
-    //        return false;
-    //    }
-
-
-    //}
 };
