7c510cd117ce285b5d3aa9e2de1bcd7ec5a1845f
zhuzi <zhuziyang123456@gmail.com>
2023-12-10 18:36:52 +0800

    add 169

---
+++ 169.cpp
@@ -0,0 +1,21 @@
+
+
+
+#include <vector>
+#include <algorithm>
+
+
+class Solution {
+public:
+ /*   int majorityElement(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        return nums[(nums.size() - 1) / 2];
+
+    }*/
+    // nt方法
+    // O(n)方法怎么做呢
+
+    // 哈希表方法显然是O(n)
+
+
+};
