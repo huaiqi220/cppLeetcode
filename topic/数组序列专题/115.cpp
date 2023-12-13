df35570660750c53030a6492a97087e344e2300f
toolbox10 <140307488010@qq.com>
2023-12-13 18:52:17 +0800

    提交最终结果

---
+++ topic/数组序列专题/115.cpp
@@ -0,0 +1,32 @@
+
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+  /*  int numDistinct(std::string s, std::string t) {
+        int count = 0;
+
+    }
+
+    void backTracking(int& count, std::string s, std::string t,int idx, std::vector<int> slist) {
+        if (idx == t.size()) {
+            count++;
+            return;
+        }
+        for (auto item : slist) {
+            std::vector<int> list = searchCharFromIndex(s, item, t[idx])
+        }
+        
+    }
+
+    std::vector<int> searchCharFromIndex(std::string& str, int idx, char c)
+    {
+        std::vector<int> res;
+        for (int i = idx; i < str.size(); i++)
+        {
+            if (str[i] == c)res.push_back(i);
+        }
+        return res;
+    }*/
+};
