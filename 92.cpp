f222eaa3afdf18e1d8d98d812fbdcc76c3296fc4
zhuzi <zhuziyang123456@gmail.com>
2023-12-13 22:22:50 +0800

    提交92，不要总以做出来了为目的啊，要学习算法思想

--- 92.cpp
+++ 92.cpp
@@ -13,51 +13,73 @@ class Solution {
         ListNode(int x, ListNode* next) : val(x), next(next) {}
     };
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode* cur = head;
-        ListNode* temp = nullptr;
-        std::vector<ListNode*> nodeVector;
-        while (cur != nullptr)
+    //ListNode* reverseBetween(ListNode* head, int left, int right) {
+    //    ListNode* cur = head;
+    //    ListNode* temp = nullptr;
+    //    std::vector<ListNode*> nodeVector;
+    //    while (cur != nullptr)
+    //    {
+    //        nodeVector.push_back(cur);
+    //        // cur = cur->next;
+
+    //        temp = cur->next;
+    //        cur->next = nullptr;
+    //        cur = temp;
+    //    }
+
+    //    while (left < right)
+    //    {
+    //        swap(nodeVector, left, right);
+    //        left++;
+    //        right--;
+    //    }
+
+    //    return vec2list(nodeVector);
+
+    //}
+
+    //void swap(std::vector<ListNode*> &nodeVector, int left, int right)
+    //{
+    //    ListNode* end = nodeVector[right - 1];
+    //    nodeVector[right - 1] = nodeVector[left - 1];
+    //    nodeVector[left - 1] = end;
+    //}
+
+    //ListNode* vec2list(std::vector<ListNode*>& nodeVector)
+    //{
+    //    ListNode* head = nodeVector[0];
+    //    ListNode* cur = nodeVector[0];
+
+    //    for (int i = 1; i < nodeVector.size();i++)
+    //    {
+    //        cur->next = nodeVector[i];
+    //        cur = nodeVector[i];
+    //    }
+
+    //    return head;
+    //}
+
+
+    /*ListNode* reverseBetween(ListNode* head, int left, int right) {
+        int count = 1;
+        ListNode* move = head;
+        while (count < left -1)
         {
-            nodeVector.push_back(cur);
-            // cur = cur->next;
-
-            temp = cur->next;
-            cur->next = nullptr;
-            cur = temp;
+            move = move->next;
+            count++;
         }
-
-        while (left < right)
+        ListNode* leftbound = move;
+        while (count < right)
         {
-            swap(nodeVector, left, right);
-            left++;
-            right--;
+            ListNode* willswitch = move->next;
+            ListNode* beswitch1 = willswitch->next;
+            leftbound->next = beswitch1;
+            willswitch->next = beswitch1->next;
+            beswitch1->next = willswitch;
+            count++;
         }
+    }*/
 
-        return vec2list(nodeVector);
-
-    }
-
-    void swap(std::vector<ListNode*> &nodeVector, int left, int right)
-    {
-        ListNode* end = nodeVector[right - 1];
-        nodeVector[right - 1] = nodeVector[left - 1];
-        nodeVector[left - 1] = end;
-    }
-
-    ListNode* vec2list(std::vector<ListNode*>& nodeVector)
-    {
-        ListNode* head = nodeVector[0];
-        ListNode* cur = nodeVector[0];
-
-        for (int i = 1; i < nodeVector.size();i++)
-        {
-            cur->next = nodeVector[i];
-            cur = nodeVector[i];
-        }
-
-        return head;
-    }
 
 };
 
